0a75f434e69bf8814ab70d73f84d9c70cb96eb7e
Las Safin <me@las.rs>
2021-11-24 20:04:43 +0000

    final commit

--- overlay.nix
+++ overlay.nix
@@ -3,35 +3,20 @@
 let
   sources = import ./sources.nix { inherit fetchFromGitHub; };
   overlay1 = final: prev: {
-    canonical-json = hlib.overrideCabal (_: { doCheck = false; broken = false; }) prev.canonical-json;
-    flat = prev.flat.overrideAttrs (_: {
-      src = fetchFromGitHub {
-        owner = "Quid2";
-        repo = "flat";
-        rev = "ee59880f47ab835dbd73bea0847dab7869fc20d8";
-        sha256 = "0000000000000000000000000000000000000000000000000000";
-      };
-    });
-    purescript-bridge = prev.purescript-bridge.overrideAttrs (_: {
-      src = fetchFromGitHub {
-        owner = "input-output-hk";
-        repo = "purescript-bridge";
-        rev = "6a92d7853ea514be8b70bab5e72077bf5a510596";
-        sha256 = "0000000000000000000000000000000000000000000000000000";
-      };
-    });
-    servant-purescript = prev.servant-purescript.overrideAttrs (_: {
-      src = fetchFromGitHub {
-        owner = "input-output-hk";
-        repo = "servant-purescript";
-        rev = "a0c7c7e37c95564061247461aef4be505a853538";
-        sha256 = "0000000000000000000000000000000000000000000000000000";
-      };
-    });
-
     dns = final.callPackage (import ./deps/dns.nix { inherit lib; }) {};
     unliftio-core = final.callPackage (import ./deps/unliftio-core.nix { inherit lib; }) {};
     #aeson_1_4_7_1 = final.callPackage (import ./deps/aeson-1.4.nix { inherit lib; }) {};
+
+    ral_0_1 = hlib.overrideCabal (_: {
+      dontCheck = true;
+      version = "0.1";
+      sha256 = "BIzytbOUIpUDvddIyTzBllC0Ij0B4R6VSzc/dZXCEkE=";
+    }) final.ral;
+
+    streaming-binary_0_2 = hlib.overrideCabal (_: {
+      version = "0.2.2.0";
+      sha256 = "z9CQA5Y0tloQJDAOyR9W89bT3HfNOuI40l1EL9jZPOE=";
+    }) final.streaming-binary;
 
     network = final.network_3_1_2_5;
 
@@ -44,6 +29,7 @@ let
     ouroboros-network = hlib.dontCheck prev.ouroboros-network;
     OddWord = hlib.dontCheck prev.OddWord;
     hs-rqlite = hlib.dontCheck prev.hs-rqlite;
+    canonical-json = hlib.overrideCabal (_: { doCheck = false; broken = false; }) prev.canonical-json;
     gray-code = hlib.overrideCabal (o: {
       doCheck = false;
       postPatch = ''
@@ -52,10 +38,24 @@ let
     }) prev.gray-code;
     # FIXME: tests
     moo = hlib.dontCheck prev.moo;
+    # FIXME: tests
+    cardano-crypto-tests = hlib.dontCheck prev.cardano-crypto-tests;
+
+    # there's a hackage package named libsodium...
+    libsodium = builtins.throw "override me";
 
     cardano-crypto-class = prev.cardano-crypto-class.override {
-      inherit libsodium; # there's a hackage package named libsodium...
+      inherit libsodium;
     };
+    cardano-crypto-praos = prev.cardano-crypto-praos.override {
+      inherit libsodium;
+    };
+    plutus-core = prev.plutus-core.override {
+      ral = final.ral_0_1;
+    };
+    cardano-ledger-byron = hlib.dontCheck (prev.cardano-ledger-byron.override {
+      streaming-binary = final.streaming-binary_0_2;
+    });
   };
   overlay2 = final: _: (import ./pkgs.nix { inherit sources; }) final;
   overlay = lib.composeExtensions overlay2 overlay1;
